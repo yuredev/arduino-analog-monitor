f5da0264bf7ae9467c861cc958cf20d858809d66
Yurematias <yurematias@ufrn.edu.br>
2019-09-30 07:53:00 -0300

    2nd potentiometer added

--- server.js
+++ server.js
@@ -2,49 +2,50 @@ const express = require("express")
 const app = express()
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
-// const five = require("johnny-five")
-// const arduino = new five.Board({ port: "COM12" })
+const five = require("johnny-five")
+const arduino = new five.Board({ port: "COM6" })
 const port = 8000
 let setPoint, v1, v2
 
 app.use(express.static(__dirname + "/client"))
 
-// arduino.on("ready", function () {
-// console.log("Arduino Pronto")
+arduino.on("ready", function () {
+	console.log("Arduino Pronto")
 
-// potenciometro = new five.Sensor({
-//     pin: "A3",
-//     freq: 250
-// })
+	pot1 = new five.Sensor({ pin: "A1", freq: 250 })
+	pot2 = new five.Sensor({ pin: "A3", freq: 250 })
 
-// arduino.repl.inject({
-//     pot: potenciometro
-// })
-
-io.on(
-	"connection",
-	socket => {
+	arduino.repl.inject({
+		pot: pot1
+	})
+	arduino.repl.inject({
+		pot: pot2
+	})
+	io.on("connection", socket => {
 		console.log("Nova conexão com o id:", socket.id)
 		socket.on("connected", msg => {
 			console.log(msg)
-			// potenciometro.on('data', function () {
-			setInterval(() => {
-				v1 = Math.random() * 5
-				v2 = Math.random() * 5
-				socket.emit("controlBit", v1 > setPoint && v2 > setPoint ? 1 : 0)
-				socket.emit("value1", v1)
-				socket.emit("value2", v2)
-			}, 300)
-			// })
+			pot1.on('data', () => {
+				setInterval(function () {
+					v1 = pot1.value * 5 / 1024
+					v2 = pot2.value * 5 / 1024
+					socket.emit("value1", v1)
+				}, 300)
+			})
+			pot2.on('data', function () {
+				setInterval(function () {
+					v2 = pot2.value * 5 / 1024
+					socket.emit("value2", v2)
+					socket.emit("controlBit", v1 > setPoint && v2 > setPoint ? 1 : 0)
+				}, 300)
+			})
 		})
 		socket.on("setPoint", msg => {
 			setPoint = msg
 			console.log(`Set point alterado para: ${setPoint} volts`)
 		})
-	},
-	1000
-)
-// })
+	}, 1000)
+})
 
 http.listen(port, () => {
 	console.log(`Servidor executando na porta: ${port}`)
