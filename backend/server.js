1e934a42cd696e1d0d3e987ae0271a4edc1886e0
Yurematias <yurematias@ufrn.edu.br>
2019-10-13 17:29:49 -0300

    backend modularizado

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ let setPoint = null; // valor de setpoint passado pelo usuário
 
 // declarando Arduino na porta ao qual está conectado
 // const arduino = new five.Board({ port: "COM6" });
-// let pot1, pot2;
+let pot1, pot2;
 // quando o arduino estiver pronto executar 
 // arduino.on('ready', function () {
 	console.log('Placa pronta');
@@ -20,17 +20,19 @@ let setPoint = null; // valor de setpoint passado pelo usuário
 		setPins();
 		socket.on('setPins', pins => setPins(pins));
 		// quando receber um novo setPoint é necessário mandar o novo set para todos os clientes 
-		socket.on('changingSetPoint', newSetPoint => changeSetPoint(newSetPoint, socket));	
+		socket.on('changingSetPoint', newSetPoint => changeSetPoint(socket, newSetPoint));	
 	});
 // });
 
 // altera o atual set point 
-function changeSetPoint(newSetPoint, socket) {
+function changeSetPoint(socket, newSetPoint) {
 	console.log('Set point mudado para ' + newSetPoint);
 	setPoint = newSetPoint;
 	socket.broadcast.emit('changeSetPoint', setPoint); // enviando para todos clientes exceto o atual
 }
 
+// função para mudar canais do arduino 
+// por padrão os canais são A0 e A1
 function setPins(pins = ['A0','A1']) {
 	// pot1 = new five.Sensor({ pin: pins[0], freq: 250 }); // primeiro potenciômetro
 	// pot2 = new five.Sensor({ pin: pins[1], freq: 250 }); // segundo potenciômetro
@@ -39,19 +41,21 @@ function setPins(pins = ['A0','A1']) {
 	console.log(`Canais setados ${pins[0]} e ${pins[1]}`);
 }
 
+// faz o servidor começar a mandar os dados para os clientes 
 function startSending(socket) {
 	console.log('Mandando dados para ' + socket.id);
 	// passar o setPoint atual para o novo usuário conectado
 	socket.emit('changeSetPoint', setPoint);
-	// pot1.on('data', () => {
-		// setInterval(() => socket.emit('v1', pot1.value * 5 / 1024), 400);
-		setInterval(() => socket.emit('v1', Math.random() * 5), 400);
-	// });
-	// pot2.on('data', () => {
-		// setInterval(() => socket.emit('v2', pot2.value * 5 / 1024), 400);
-		setInterval(() => socket.emit('v2', Math.random() * 5), 400);
+	potSend(socket, pot1, 'v1');
+	potSend(socket, pot2, 'v2');
+}
+
+// manda os dados dos potenciômetros 
+function potSend(socket, pot, socketMsg) {
+	// pot.on('data', () => {
+		// setInterval(() => socket.emit(socketMsg, pot.value * 5 / 1024), 400);
+		setInterval(() => socket.emit(socketMsg, Math.random() * 5), 400);
 	// });
 }
 
-// ouvir na porta declarada 
 http.listen(port, () => console.log('Abrir em: http://localhost:' + port));
