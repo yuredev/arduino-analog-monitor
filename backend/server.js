fe17d21e5b76013734856a2c131cbf67021f53b2
Yurematias <yurematias@ufrn.edu.br>
2019-10-14 14:44:30 -0300

    volta que deu ruim

--- backend/server.js
+++ backend/server.js
@@ -8,54 +8,49 @@ const path = require('path'); // será utilizado para fazer o express reconhecer
 const port = 8080;
 app.use(express.static(path.resolve(__dirname + "/../frontend"))); // atender requisições com pasta a frontend
 let setPoint = null; // valor de setpoint passado pelo usuário  
+let pinWasInitialize = false
 
 // declarando Arduino na porta ao qual está conectado
-// const arduino = new five.Board({ port: "COM6" });
+const arduino = new five.Board({ port: "COM6" });
 let pot1, pot2;
 // quando o arduino estiver pronto executar 
-// arduino.on('ready', function () {
+arduino.on('ready', function () {
 	console.log('Placa pronta');
 	io.on('connection', socket => { 
-		startSending(socket);
-		setPins();
+		if(!pinWasInitialize)
+			setPins();
 		socket.on('setPins', pins => setPins(pins));
-		// quando receber um novo setPoint é necessário mandar o novo set para todos os clientes 
-		socket.on('changingSetPoint', newSetPoint => changeSetPoint(socket, newSetPoint));	
+		socket.on('clientReady', clientId => startSending(socket, clientId));	
 	});
-// });
+});
 
-// altera o atual set point 
-function changeSetPoint(socket, newSetPoint) {
-	console.log('Set point mudado para ' + newSetPoint);
-	setPoint = newSetPoint;
-	socket.broadcast.emit('changeSetPoint', setPoint); // enviando para todos clientes exceto o atual
-}
-
-// função para mudar canais do arduino 
-// por padrão os canais são A0 e A1
 function setPins(pins = ['A0','A1']) {
-	// pot1 = new five.Sensor({ pin: pins[0], freq: 250 }); // primeiro potenciômetro
-	// pot2 = new five.Sensor({ pin: pins[1], freq: 250 }); // segundo potenciômetro
-	// arduino.repl.inject({ pot: pot1 });
-	// arduino.repl.inject({ pot: pot2 });
+	pot1 = new five.Sensor({ pin: pins[0], freq: 250 }); // primeiro potenciômetro
+	pot2 = new five.Sensor({ pin: pins[1], freq: 250 }); // segundo potenciômetro
+	arduino.repl.inject({ pot: pot1 });
+	arduino.repl.inject({ pot: pot2 });
 	console.log(`Canais setados ${pins[0]} e ${pins[1]}`);
+	pinWasInitialize = true;
 }
 
-// faz o servidor começar a mandar os dados para os clientes 
-function startSending(socket) {
-	console.log('Mandando dados para ' + socket.id);
+function startSending(socket, clientId) {
+	console.log('Mandando dados para ' + clientId);
 	// passar o setPoint atual para o novo usuário conectado
 	socket.emit('changeSetPoint', setPoint);
-	potSend(socket, pot1, 'v1');
-	potSend(socket, pot2, 'v2');
+	// quando receber um novo setPoint é necessário mandar o novo set para todos os clientes 
+	socket.on('changingSetPoint', newSetPoint => {
+		setPoint = newSetPoint;
+		socket.broadcast.emit('changeSetPoint', setPoint); // enviando para todos clientes exceto o atual 
+	});
+	pot1.on('data', () => {
+		setInterval(() => socket.emit('v1', pot1.value * 5 / 1024), 400);
+		// setInterval(() => socket.emit('v1', Math.random() * 5), 400);
+	});
+	pot2.on('data', () => {
+		setInterval(() => socket.emit('v2', pot2.value * 5 / 1024), 400);
+		// setInterval(() => socket.emit('v2', Math.random() * 5), 400);
+	});
 }
 
-// manda os dados dos potenciômetros 
-function potSend(socket, pot, socketMsg) {
-	// pot.on('data', () => {
-		// setInterval(() => socket.emit(socketMsg, pot.value * 5 / 1024), 400);
-		setInterval(() => socket.emit(socketMsg, Math.random() * 5), 400);
-	// });
-}
-
+// ouvir na porta declarada 
 http.listen(port, () => console.log('Abrir em: http://localhost:' + port));
--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 const socket = io();           // constante que armazenará o objeto do socket.io
 const startTime = new Date(); // armazenar o tempo inicial ao executar em milisegundos 
 let value1 = null, value2 = null, controlBitValue = null; // valores de y inseridos nos gráficos  
-let setPoint;                 // determina o valor do set point do primeiro gráfico 
+let setPoint;         // determina o valor do set point do primeiro gráfico 
 let showBitGraph = false;    // determina se o gráfico do set point será mostrado 
 let pause = false;           // determina se o gráfico está pausado 
 let x = 0;                   // x representa os pontos no eixo x do gráfico
@@ -11,36 +11,40 @@ let secP;                     // armazenar o tempo passado (secP = seconds passe
 let scaleMin = 0, scaleMax = 5; // escalas do primeiro gráfico 
 let option = 'Voltagem';        // gráfico a ser exibido 
 let executingGraph, executingGraphCB;  // armazenará dos dois gráficos 
-let pins = ['A0', 'A1']                     // representam os canais de entrada do arduino
 let layout = {                 // layout a ser usado nos gráficos 
     height: 250,
     autosize: true,
     margin: { b: 50, t: 30 }
 };
+
 // array de linhas do primeiro gráfico
 let traces = [new Trace('potenciômetro 1', value1, '#D00'),
-    new Trace('potenciômetro 2', value1, 'orange'),
-    new Trace('set point', setPoint, '00A')
+new Trace('potenciômetro 2', value1, 'orange'),
+new Trace('set point', setPoint, '00A')
 ];
 // array de linhas do gráfico do bit de controle (CB: Bit Control)
 let traceCB = [new Trace('bit de controle', controlBitValue)];
 
-window.onload = initialize;
+window.onload = initialize
 
 // inicializar a aplicação
-function initialize() {    
+function initialize() {
+    socket.on('connect', () => {
+        socket.emit('clientReady', socket.id);
+    })
     startPloting();
     startSocketListening();
-    startPinsListening();
     $('#' + option).addClass('marked');      // marcar a opção atual do gráfico
     $('#controlBit').prop('checked', false); // deixar o checkbox desmarcado por padrão via jquery  
 }
+
 // faz o cliente começar a ouvir os dados do servidor 
 function startSocketListening() {
     socket.on('v1', receivedData => value1 = receivedData);
     socket.on('v2', receivedData2 => value2 = receivedData2);
     socket.on('changeSetPoint', newSetPoint => setPoint = newSetPoint);
 }
+
 // começa a plotar os gráficos dinamicamente
 function startPloting() {
     Plotly.plot('chart', traces, layout, { responsive: true });      // plotar primeiro gráfico 
@@ -48,30 +52,11 @@ function startPloting() {
     executingGraph = setInterval(updateGraph, 100);
     executingGraphCB = setInterval(updateGraphCB, 100);
 }
-// adiciona um ouvidor para um dos select, pinNumber determina qual será 
-function addPinListening(pinNumber) {
-    $(`#pin${pinNumber}`).val($(`#pin${pinNumber} option[value="A${pinNumber}"]`).val());
-    $(`#pin${pinNumber}`).change(() => {
-        pins[pinNumber] = $(`#pin${pinNumber} option:selected`).val();
-        if (pins[0] != pins[1]) {
-            console.log(`${pinNumber + 1}° canal mudado para ${pins[pinNumber]}`);
-            socket.emit('setPins', pins);
-        } else {
-            swal({
-                icon: "warning",
-                title: 'Você deve selecionar dois canais diferentes!!',
-                timer: 2000,
-                buttons: false,
-                dangerMode: true,
-            });
-        }
-    });
+
+function changePins() {
+    window.location.href = 'setpins.html'
 }
-// faz os selects dos pinos começarem a ouvirem mudanças de estado das options 
-function startPinsListening() {
-    addPinListening(0);
-    addPinListening(1);
-}
+
 // função construtora para gerar objetos do tipo linha 
 function Trace(name = 'unnamed trace', valueTrace, color = '#000') {
     this.name = name;
@@ -115,20 +100,26 @@ function pauseResume() {
         executingGraphCB = setInterval(updateGraphCB, 100); // retoma segundo gráfico 
     }
 }
-// faz o tempo passar, necessário para exibir o tempo abaixo dos gráficos 
-function passTime() {
+// update do primeiro gráfico 
+function updateGraph() {
+    Plotly.extendTraces('chart', { y: [[value1], [value2], [setPoint]] }, [0, 1, 2]);
     secP = secondsPassed();
+    x++;
     if (secP - cntSec > 59) {
         cntSec += 60;
         minutes++;
     }
-}
-// update do primeiro gráfico 
-function updateGraph() {
-    Plotly.extendTraces('chart', { y: [[value1], [value2], [setPoint]] }, [0, 1, 2]);
-    x++;
-    passTime();
-    graphRelayout('chart', 'voltagem', scaleMin, scaleMax);
+    Plotly.relayout('chart', {
+        xaxis: {
+            showticklabels: false,
+            title: `tempo percorrido: ${minutes}:${secP < 60 ? secP : secP - cntSec}`,
+            range: [x - 50, x],
+        },
+        yaxis: {
+            title: 'voltagem',
+            range: [scaleMin, scaleMax]
+        },
+    });
     if (value1 != null)
         document.getElementById('volts1').innerHTML = `1° potenciômetro: ${value1.toFixed(2)} volts`;
     if (value2 != null)
@@ -137,19 +128,15 @@ function updateGraph() {
 // update do gráfico de bit de controle 
 function updateGraphCB() {
     Plotly.extendTraces('chart2', { y: [[controlBitValue]] }, [0]);
-    graphRelayout('chart2', 'bit de controle', -0.5, 1.5);
-}
-// redesenha o gráfico 
-function graphRelayout(divName, graphName, rangeMin, rangeMax) {
-    Plotly.relayout(divName, {
+    Plotly.relayout('chart2', {
         xaxis: {
             showticklabels: false,
             title: `tempo percorrido: ${minutes}:${secP < 60 ? secP : secP - cntSec}`,
             range: [x - 50, x],
         },
         yaxis: {
-            title: graphName,
-            range: [rangeMin, rangeMax]
+            title: 'bit de controle',
+            range: [-0.5, 1.5]
         },
     });
 }
